a5ec7eb17c6c127bc4690cafb54f2e15f73085b8
Muataz Khataan <muataz.khataan@gmail.com>
2018-04-19 12:47:44 +0300

    Muataz
    OK

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private CharacterController _controller;
+    [SerializeField]
+    private float _speed = 3.5f;
+    private float _gravity = 9.81f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+        Vector3 velocity = direction * _speed;
+        velocity.y -= _gravity;
+
+        velocity = transform.transform.TransformDirection(velocity);
+        _controller.Move(velocity * Time.deltaTime);
+    }
+}
