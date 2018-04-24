63512f136e168b1ae8efd941bd786376950a8f90
Muataz Khataan <muataz.khataan@gmail.com>
2018-04-24 23:48:33 +0300

    Finish

---
+++ Assets/Game/Scripts/Player.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+    [SerializeField]
+    private float _speed = 3.5f;
+    private float _gravity = 9.81f;
+    [SerializeField]
+    private GameObject _muzzleFlash;
+    [SerializeField]
+    private GameObject _hitMarkerPrefab;
+    [SerializeField]
+    private AudioSource _weaponAudio;
+
+    [SerializeField]
+    private int currentAmmo;
+    private int maxAmmo = 50;
+
+    private bool _isReloading = false;
+
+    private UIManager _uiManager;
+
+    // Variable for has Coin
+    public bool hasCoin = false;
+
+    // Variable for has Weapon
+    [SerializeField]
+    private GameObject _weapon;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _controller = GetComponent<CharacterController>();
+        // Hide mouse cursor
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        currentAmmo = maxAmmo;
+
+        _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // If left click
+        // Case ray from center point of main camera
+
+        if (Input.GetMouseButton(0) && currentAmmo > 0 && _weapon.activeSelf)
+        {
+            Shoot();
+        }
+        else
+        {
+            _muzzleFlash.SetActive(false);
+            _weaponAudio.Stop();
+        }
+
+        // Reload by Key R
+        if (Input.GetKeyDown(KeyCode.R) && _isReloading == false)
+        {
+            _isReloading = true;
+            StartCoroutine(Reload());
+        }
+
+        // Reload by Mouse
+        if (Input.GetMouseButton(1) && _isReloading == false)
+        {
+            _isReloading = true;
+            StartCoroutine(Reload());
+        }
+
+        // If Escape key pressed
+        // Unhide mouse cursor
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        CalculateMovement();
+    }
+
+    void Shoot()
+    {
+        _muzzleFlash.SetActive(true);
+        currentAmmo--;
+        _uiManager.UpdateAmmo(currentAmmo);
+        // if audio not playing 
+        // play audio
+        if (_weaponAudio.isPlaying == false)
+        {
+            _weaponAudio.Play();
+
+        }
+        Ray rayOrgions = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(rayOrgions, out hitInfo))
+        {
+            Debug.Log(message: "RayCast Hit: " + hitInfo.transform.name);
+            GameObject hitMarker = Instantiate(_hitMarkerPrefab, hitInfo.point, Quaternion.LookRotation(hitInfo.normal)) as GameObject;
+            Destroy(hitMarker, 1f);
+
+            // check if we hit the crate
+            Destructable crate = hitInfo.transform.GetComponent<Destructable>();
+            if(crate != null)
+            {
+                crate.DestroyCrate();
+            }
+            // Destroy Crate
+        }
+
+    }
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+        Vector3 velocity = direction * _speed;
+        velocity.y -= _gravity;
+
+        velocity = transform.transform.TransformDirection(velocity);
+        _controller.Move(velocity * Time.deltaTime);
+    }
+
+    IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(1.5f);
+        currentAmmo = maxAmmo;
+        _uiManager.UpdateAmmo(currentAmmo);
+        _isReloading = false;
+    }
+
+    public void EnableWeapons()
+    {
+        _weapon.SetActive(true);
+    }
+}
---
+++ Assets/Game/Scripts/SharkShop.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkShop : MonoBehaviour {
+
+    [SerializeField]
+    private AudioClip _getOut;
+
+    // Check fo a collision
+    private void OnTriggerStay(Collider other)
+    {
+        // Check if player
+        if (other.tag == "Player")
+        {
+            // Check for E key
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Player player = other.GetComponent<Player>();
+                // Remove coin from player
+                if (player.hasCoin == true)
+                {
+                    player.hasCoin = false;
+                    // Update the inventory display
+                    UIManager uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+                    if (uiManager != null)
+                    {
+                        uiManager.RemoveCoin();
+                    }
+
+                    // Play win sound
+                    AudioSource audio = GetComponent<AudioSource>();
+                    audio.Play();
+                    player.EnableWeapons();
+                }
+                // Debug Get out of here! You do not have coin
+                else
+                {
+                    Debug.Log("Get out of here! You do not have coin.");
+                    // Play sound
+                    // Play coin Sound
+                    AudioSource.PlayClipAtPoint(clip: _getOut, position: transform.position, volume: 1f);
+                }
+            }
+        }
+    }
+}
